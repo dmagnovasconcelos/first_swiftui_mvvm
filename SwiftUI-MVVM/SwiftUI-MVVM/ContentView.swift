5158de549385fb9386a58ffe5ebe7d47abb42531
Danilo Magno de Oliveira Vasconcelos <dmagno.vasconcelos@gmail.com>
2022-02-09 12:13:10 -0300

    :sparkles: feat(ContentView) "Hello, world" - MVVM

--- SwiftUI-MVVM/SwiftUI-MVVM/ContentView.swift
+++ SwiftUI-MVVM/SwiftUI-MVVM/ContentView.swift
@@ -8,14 +8,42 @@
 import SwiftUI
 
 struct ContentView: View {
+    @ObservedObject private var model: ContentViewModel
+    
+    init(model: ContentViewModel) {
+        self.model = model
+    }
+    
     var body: some View {
-        Text("Hello, world!")
+        Text(model.state.isLoading ? "Loading..." : model.state.message)
             .padding()
+            .onAppear(perform: model.loadData)
+    }
+}
+
+struct ContenteViewState {
+    var isLoading = false
+    var message = ""
+}
+
+class ContentViewModel: ObservableObject {
+    @Published var state: ContenteViewState
+    
+    init(initialState: ContenteViewState = .init()) {
+        state = initialState
+    }
+    
+    func loadData() {
+        state.isLoading = true
+        DispatchQueue.main.asyncAfter(deadline: .now() + .seconds(2)) {
+            self.state.isLoading = false
+            self.state.message = "Hello, world!"
+        }
     }
 }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
-        ContentView()
+        ContentView(model: .init())
     }
 }
